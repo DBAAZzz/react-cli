85dc2932c735a50006c3217d43702bd13a8ef2dd
DBAAZzz <1292094030@qq.com>
2022-02-01 15:36:48 +0800

    Fix 修改了 react-chartjs-2 图表的用法

--- src/pages/Statistics/index.js
+++ src/pages/Statistics/index.js
@@ -3,32 +3,26 @@ import { Line } from 'react-chartjs-2'
 import styles from './style.module.scss'
 
 const Recommand = () => {
-    const [chartData, setChartData] = useState({})
-    const chart = () => {
-        setChartData({
-            labels: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
-            datasets: [
-                {
-                    label: '金额',
-                    data: [16, 20, 8, 26, 8, 20, 80],
-                    fill: true,
-                    backgroundColor: 'rgba(299, 11, 103, 0.1)',
-                    borderWidth: 1,
-                    borderColor: 'rgb(75, 192, 192, 1)',
-                    tension: 0.4
-                }
-            ],
-            options: {
-                responsive: true,
-                interaction: {
-                    intersect: false,
-                },
+    const [chartData, setChartData] = useState({
+        labels: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+        datasets: [
+            {
+                label: '金额',
+                data: [16, 20, 8, 26, 8, 20, 80],
+                fill: true,
+                backgroundColor: 'rgba(299, 11, 103, 0.1)',
+                borderWidth: 1,
+                borderColor: 'rgb(75, 192, 192, 1)',
+                tension: 0.4
             }
-        })
-    }
-    useEffect(() => {
-        chart()
-    }, [])
+        ],
+        options: {
+            responsive: true,
+            interaction: {
+                intersect: false,
+            },
+        }
+    })
     return (
         <div>
             <div className={styles.text}>
